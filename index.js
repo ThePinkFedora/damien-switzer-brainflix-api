63d9e6ae1adb1f6a553159626046ad55c599bfbd
Damien Switzer <damienswitzer@hotmail.com>
2023-04-17 13:39:10 -0700

    Refactor json data and implement video posting

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
 const fs = require("node:fs");
 const express = require("express");
 const app = express();
+const { v4 } = require("uuid");
 
 const readVideosData = () => JSON.parse(fs.readFileSync("./data/videos.json"));
-const writeVideosData = (data) => fs.writeFileSync("./data/videos.json", data);
+const writeVideosData = (data) => fs.writeFileSync("./data/videos.json", JSON.stringify(data));
+
+function Video(title = "", channel = "", image = "") {
+  this.id = v4();
+  this.title = title;
+  this.channel = channel;
+  this.image = image;
+  this.views = "0";
+  this.likes = "0";
+  this.duration = "0:00";
+  this.timestamp = new Date().getTime();
+  this.comments = [];
+}
 
 app.get("/", (req, res) => {
   const indexFile = fs.readFileSync("./index.html", "utf8");
   res.send(indexFile);
 });
 
-app.get("/videos", (req, res) => {
-  const { videos } = readVideosData();
-  res.json(videos);
-});
+app
+  .route("/videos")
+  .get((req, res) => {
+    const videos = readVideosData().map((video) => ({
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: video.image,
+    }));
+    res.json(videos);
+  })
+  .post((req, res) => {
+    const videos = readVideosData();
+    const newVideo = new Video(`New Video (${videos.length + 1})`, "BrainFlix User", "/images/image0.jpeg");
+    videos.push(newVideo);
+    writeVideosData(videos);
+
+    res.status(201);
+    res.send({ id: newVideo.id });
+  });
 
 app.get("/videos/:videoId", (req, res) => {
   const { videoId } = req.params;
-  const { videoDetails: videoDetailsList } = readVideosData();
-  const video = videoDetailsList.find((video) => video.id === videoId);
+  const videos = readVideosData();
+  const video = videos.find((video) => video.id === videoId);
   res.json(video);
 });
 
