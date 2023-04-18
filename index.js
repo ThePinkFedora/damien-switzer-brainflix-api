3a8cfdc34ea885bb7017cc740d5329688c63bc69
Damien Switzer <damienswitzer@hotmail.com>
2023-04-18 09:57:15 -0700

    Configure middleware
    -Setup /videos route middleware
    -Setup cors
    -Setup static

--- index.js
+++ index.js
@@ -1,68 +1,18 @@
 const fs = require("node:fs");
 const express = require("express");
 const app = express();
-const { v4 } = require("uuid");
+const cors = require("cors");
+const videosRoutes = require("./routes/videos");
 
-const readVideosData = () => JSON.parse(fs.readFileSync("./data/videos.json"));
-const writeVideosData = (data) => fs.writeFileSync("./data/videos.json", JSON.stringify(data));
-
-function Video(title, channel, image, description) {
-  this.id = v4();
-  this.title = title;
-  this.channel = channel;
-  this.description = description;
-  this.image = image;
-  this.views = "0";
-  this.likes = "0";
-  this.duration = "0:00";
-  this.video = "https://project-2-api.herokuapp.com/stream";
-  this.timestamp = new Date().getTime();
-  this.comments = [];
-}
-
+app.use(cors());
 app.use(express.json());
+app.use("/static", express.static("public"));
+
+app.use("/videos", videosRoutes);
 
 app.get("/", (req, res) => {
   const indexFile = fs.readFileSync("./index.html", "utf8");
   res.send(indexFile);
-});
-
-app
-  .route("/videos")
-  .get((req, res) => {
-    const videos = readVideosData().map((video) => ({
-      id: video.id,
-      title: video.title,
-      channel: video.channel,
-      image: video.image,
-    }));
-    res.json(videos);
-  })
-  .post((req, res) => {
-    const videos = readVideosData();
-    const { title, channel, image, description } = req.body;
-
-    if (!title || !description) {
-      res.status(400);
-      res.json({
-        error: "POST body must contain all requiredProperties",
-        requiredProperties: ["title", "description"],
-      });
-    }
-
-    const newVideo = new Video(title, channel ?? "BrainFlix User", image ?? "", description);
-    videos.push(newVideo);
-    writeVideosData(videos);
-
-    res.status(201);
-    res.send({ id: newVideo.id });
-  });
-
-app.get("/videos/:videoId", (req, res) => {
-  const { videoId } = req.params;
-  const videos = readVideosData();
-  const video = videos.find((video) => video.id === videoId);
-  res.json(video);
 });
 
 app.listen(80, () => console.log("Server is running: " + new Date()));
